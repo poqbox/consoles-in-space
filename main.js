e729e6e4d14e86b33a025a16dbf796f82631480f
poq <120293384+poqbox@users.noreply.github.com>
2025-06-06 20:27:47 -0400

    Refactored spaceBattles to be a function

--- main.js
+++ main.js
@@ -3,17 +3,25 @@ import * as hud from "./hud.js"
 
 
 var jsAssembly = new spaceObjects.USSAssembly("JS Assembly")
-var spaceBattles = {
+var space_battles = {
     europaA1: new spaceObjects.spaceBattle("Europa A1", 6)
 }
 const spaceBattles_as_text = "    " + Object.keys(spaceBattles).join("\n    ")
 
 
+function spaceBattles() {
+    let available_battles = "Available space battles:\n"
+    for (const key in space_battles)
+        available_battles += `  ${key}\n`
+    console.log(available_battles)
+}
+
+
 // Add to global scope
 globalThis.jsAssembly = jsAssembly
 globalThis.spaceBattles = spaceBattles
-for (const key in spaceBattles)
-    globalThis[key] = spaceBattles[key]
+for (const key in space_battles)
+    globalThis[key] = space_battles[key]
 globalThis.commands = hud.Lexicon.commands
 globalThis.firstPage = hud.Lexicon.firstPage
 globalThis.lastPage = hud.Lexicon.lastPage
